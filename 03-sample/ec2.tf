24a6a8df8179a711eb03d3ef6ab93dd206304150
Vineeth <vineethbandlamudi@gmail.com>
2021-09-06 15:39:35 +0530

    ec2 with one sg

---
+++ 03-sample/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "sample" {
+  ami                       = "ami-074df373d6bafa625"
+  instance_type             = "t2.micro"
+  vpc_security_group_ids    = []
+  tags = {
+    Name                    =  "Sample"
+  }
+}
